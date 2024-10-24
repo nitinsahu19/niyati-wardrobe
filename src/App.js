7cefabd38462807ba20c665b872946d497a9d431
Nitin Sahu <sahunitin900@gmail.com>
2024-10-24 23:59:00 +0530

    react-routing introduced

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import "./App.css";
-import Homepage from './pages/homepage/homepage.component'
+import Homepage from "./pages/homepage/homepage.component";
+import { Route, Routes } from "react-router-dom";
+
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Its a Hats page</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/hats" element={<HatsPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { useNavigate, useLocation } from "react-router-dom";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
-    <div
-      style={{ backgroundImage: `url(${imageUrl})` }}
-      className="background-image"
-    />
-    <div className="content">
-      <h1 className="title">{title.toUpperCase()}</h1>
-      <span className="subtitle">SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleClick = () => {
+    const newUrl = `${location.pathname}${linkUrl}`;
+    navigate(newUrl);
+  };
+
+  return (
+    <div className={`${size} menu-item`} onClick={handleClick}>
+      <div
+        style={{ backgroundImage: `url(${imageUrl})` }}
+        className="background-image"
+      />
+      <div className="content">
+        <h1 className="title">{title.toUpperCase()}</h1>
+        <span className="subtitle">SHOP NOW</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default MenuItem;
