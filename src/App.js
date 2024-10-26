d30ade22aa523c9074060f8a768b731a9e3978cc
Nitin Sahu <sahunitin900@gmail.com>
2024-10-27 00:06:12 +0530

    created shop-page with few components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import "./App.css";
 import Homepage from "./pages/homepage/homepage.component";
 import { Route, Routes } from "react-router-dom";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1>Its a Hats page</h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/hats" element={<HatsPage />} />
+        <Route path="/shop" element={<ShopPage />} />
       </Routes>
     </div>
   );
