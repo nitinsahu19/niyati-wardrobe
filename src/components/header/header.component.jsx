67e6bdc6a8ee146724fea119783c06053c380dde
Nitin Sahu <sahunitin900@gmail.com>
2024-11-07 15:09:35 +0530

    added cartIcon and cart-dropdown component in the application

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,9 +4,11 @@ import { Link } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.utils";
 import { connect } from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 // currentUser is passed as a prop to determine whether to show "SIGN IN" or "SIGN OUT."
-const Header = ({ currentUser }) => {
+const Header = ({ currentUser, hidden }) => {
   return (
     <div className="header">
       <Link className="logo-container" to="/">
@@ -31,13 +33,20 @@ const Header = ({ currentUser }) => {
             </Link>
           )}
         </Link>
+
+        {/* Cart icon */}
+        <CartIcon />
       </div>
+
+      {hidden ? null : <CartDropdown />}
     </div>
   );
 };
 
 // mapStateToProps is used to access the currentUser from the Redux store.
-const mapStateToProps = (state) => ({ currentUser: state.user.currentUser });
+const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
+  currentUser,
+  hidden,
+});
 
 export default connect(mapStateToProps)(Header);
- 
--- src/redux/user/user.types.js
+++ src/redux/user/user.types.js
@@ -1,3 +1,3 @@
 export const UsetActionTypes = {
-  SET_CURRENT_USER: SET_CURRENT_USER,
+  SET_CURRENT_USER: "SET_CURRENT_USER",
 };
