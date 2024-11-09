2d5df9309ba12629edcc509b8840dbc97de02f54
Nitin Sahu <sahunitin900@gmail.com>
2024-11-09 12:31:58 +0530

    added cartUtils to group the cartItems, & upadated the syles & previous code

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,19 +1,27 @@
 import { CartActionTypes } from "./cart.types";
+import { addItemToCart } from "./cart.utils";
 
 const INITAIL_STATE = {
   hidden: true,
+  cartItems: [],
 };
 
 const cartReducer = (state = INITAIL_STATE, action) => {
-    switch (action.type) {
-        case CartActionTypes.TOGGLE_CART_HIDDEN:
-            return {
-                ...state,
-                hidden: !state.hidden 
-            }
-            default:
-                return state
-    }
-}
+  switch (action.type) {
+    case CartActionTypes.TOGGLE_CART_HIDDEN:
+      return {
+        ...state,
+        hidden: !state.hidden,
+      };
 
-export default cartReducer
+    case CartActionTypes.ADD_ITEM:
+      return {
+        ...state,
+        cartItems: addItemToCart(state.cartItems, action.payload),
+      };
+    default:
+      return state;
+  }
+};
+
+export default cartReducer;
